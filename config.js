ca8c28da106adf42435a7828d4d3e3c4c5839abe
Geoffrey <vectorofficeautomation@gmail.com>
2020-04-18 02:29:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const callbacks = providers.map(provider => {
 const [twitterURL, googleURL, facebookURL, githubURL] = callbacks
 
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production'
-  ? 'https://react-auth-twitter.netlify.com'
+  ? 'https://trackfree247.com'
   : ['https://127.0.0.1:3000', 'https://localhost:3000']
 
 exports.TWITTER_CONFIG = {
